db25d76577ed443c4c2ea350f43c0a49fc79d08e
Ankit-865 <ankitn171@gmail.com>
2025-07-31 12:35:02 +0530

    🔧 Restructure Day1 files with renamed scripts

---
+++ day1/1_quote_generator.py
@@ -0,0 +1,11 @@
+import random 
+quotes = [
+    "You are capable of amazing things.",
+    "Push yourself, because no one else is going to do it.",
+    "Success is not final, failure is not fatal.",
+    "Dream big and dare to fail.",
+    "Consistency is more important than perfection."
+]
+
+for _ in range (5):
+    print(random.choice(quotes))
